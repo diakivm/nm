bafa6743357dcd4fa9da6776a1e4613bdba996f8
diakiwm <diakiwm@gmail.com>
2023-03-02 16:06:05 +0200

    Added ip whitelist

--- config/config.js
+++ config/config.js
@@ -1,9 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 500,
     MONGO_DB: process.env.MONGO_URL || 'mongodb://localhost:27017',
-
+    CORS_DOMAINS: process.env.CORS_DOMAINS || "",
     ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
-
     TRANSACTIONS_COUNT_ON_INIT: +process.env.TRANSACTIONS_COUNT_ON_INIT || 100,
     SLEEP_TIME_ON_LOAD_TRANSACTIONS: +process.env.SLEEP_TIME_ON_LOAD_TRANSACTIONS || 3000,
 };
