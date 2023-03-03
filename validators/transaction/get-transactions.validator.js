67c947ee0c62b94b60b76ba82673f29eb56f1eab
diakiwm <diakiwm@gmail.com>
2023-03-03 11:00:01 +0200

    Changed find field

--- validators/transaction/get-transactions.validator.js
+++ validators/transaction/get-transactions.validator.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 module.exports = Joi.object({
     fromAddress: Joi.string(),
     toAddress: Joi.string(),
-    id: Joi.string().regex(/^[0-9a-fA-F]{24}$/),
+    id: Joi.string(),
     blockNumber: Joi.number().integer().min(0),
     page: Joi.number().integer().min(1),
     limit: Joi.number().integer().min(1).max(100),
